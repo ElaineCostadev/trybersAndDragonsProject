5f6b87c0a604313742347f5f59eccbc617315fb6
ElaineCosta <elainerscosta@hotmail.com>
2022-09-23 19:43:35 -0300

    :fix: ajustando ciclo de dependencia.

--- src/Fighter/SimpleFighter.ts
+++ src/Fighter/SimpleFighter.ts
@@ -1,9 +1,7 @@
-import Fighter from './Fighter';
-
 interface SimpleFighter {
   lifePoints: number,
   strength: number,
-  attack(enemy: Fighter | SimpleFighter): void;
+  attack(enemy: SimpleFighter): void;
   receiveDamage(attackPoints: number): number
 }
 
