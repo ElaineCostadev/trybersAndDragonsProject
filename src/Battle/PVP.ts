1eff036d7354cb75bbba0a21ef32491bc743fc00
ElaineCosta <elainerscosta@hotmail.com>
2022-09-26 12:25:38 -0300

    :fix: retirando o super.fight do constructor e refatorando a logica da funcao fight

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -6,7 +6,6 @@ class PVP extends Battle {
   private _player2: Fighter;
   constructor(player1: Fighter, player2: Fighter) {
     super(player1);
-    super.fight();
     this._player1 = player1;
     this._player2 = player2;
   }
@@ -20,12 +19,12 @@ class PVP extends Battle {
   }
 
   fight(): number {
-    this._player1.attack(this.player2);
-    this._player2.attack(this.player1);
-
-    if (this._player1.lifePoints === -1 || this._player2.lifePoints === -1) {
-      return super.fight();
+    // ajuda do Leo - mentoria - refatoração
+    while (this._player1.lifePoints !== -1 && this._player2.lifePoints !== -1) {
+      this._player1.attack(this.player2);
+      this._player2.attack(this.player1);
     }
+    // outra possivel forma de escrever a condição - !monster1.lifePoints === -1 || !monster2.lifePoints === -1
     return super.fight();
   }
 }
